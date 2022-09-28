84d788eadfefba9aac44a43aedae05421073e72e
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2022-09-29 00:38:54 +0300

    add exchange-button, fix enter minus

--- src/components/converter/Converter.tsx
+++ src/components/converter/Converter.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useAppSelector } from '../../hooks/redux';
 import { selectRates } from '../../store/selectors/rates';
 import Loader from '../loader/Loader';
+import { FaExchangeAlt } from 'react-icons/fa';
 
 import './converter.scss';
 import Select from './select/Select';
@@ -33,8 +34,19 @@ const Converter: React.FC<{}> = () => {
     });
   };
 
+  const clickHandler = () => {
+    setCurrencies({
+      firstCur: currencies.secondCur,
+      secondCur: currencies.firstCur,
+    });
+  };
+
   const changeFistCountHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setFirstCount(event.target.value.replace(/^0+/, ''));
+    if (event.target.value[0] === '-') {
+      setSecondCount('');
+      return;
+    }
     const secondCountValue =
       (+event.target.value * ratesObj[currencies.firstCur]) / ratesObj[currencies.secondCur];
     setSecondCount(secondCountValue.toFixed(2));
@@ -42,6 +54,10 @@ const Converter: React.FC<{}> = () => {
 
   const changeSecondCountHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setSecondCount(event.target.value.replace(/^0+/, ''));
+    if (event.target.value[0] === '-') {
+      setFirstCount('');
+      return;
+    }
     const firstCountValue =
       (+event.target.value * ratesObj[currencies.secondCur]) / ratesObj[currencies.firstCur];
     setFirstCount(firstCountValue.toFixed(2));
@@ -53,7 +69,13 @@ const Converter: React.FC<{}> = () => {
       {isLoading && <Loader />}
       <div className='converter__container'>
         <div className='converter__firstCur'>
-          <input onChange={changeFistCountHandler} type='number' value={firstCount} />
+          <input
+            onChange={changeFistCountHandler}
+            type='number'
+            value={firstCount}
+            max={9999999999}
+            min={0}
+          />
           <Select
             selectCurHandle={selectCurHandle}
             rates={rates}
@@ -61,8 +83,17 @@ const Converter: React.FC<{}> = () => {
             value={currencies.firstCur}
           />
         </div>
+        <button onClick={clickHandler} className='converter__button'>
+          <FaExchangeAlt />
+        </button>
         <div className='converter__secondCur'>
-          <input onChange={changeSecondCountHandler} type='number' value={secondCount} />
+          <input
+            onChange={changeSecondCountHandler}
+            type='number'
+            value={secondCount}
+            max={9999999999}
+            min={0}
+          />
           <Select
             selectCurHandle={selectCurHandle}
             rates={rates}
